1e420ce007b238ed6ec268bb24a82fa0181e32fb
Dev-Shaharyar <shaharyar@instaview.ai>
2025-01-15 14:45:59 +0530

    Modified the UT service layer

--- pkg/application/user/user_data_test.go
+++ pkg/application/user/user_data_test.go
@@ -2,11 +2,18 @@ package user
 
 import "github.com/Crud-application/pkg/domain/userAgg"
 
-var TestUserModelData = CreateSampleUser(
-	"mocked-uuid",
+var userAggData1 = CreateSampleUser(
+	"mocked-uuid1",
+	"shaharyar alam",
+	"shaharyar@example.com",
+	123456789,
+)
+
+var userAggData2 = CreateSampleUser(
+	"mocked-uuid2",
 	"John Doe",
 	"johndoe@example.com",
-	123456789,
+	987654321,
 )
 
 func CreateSampleUser(id, name, email string, phone_number int64) userAgg.User {
